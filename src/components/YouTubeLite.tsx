a2e3900fc1deae31c9549d9831c1523d204ea787
Corey Magin <thefancypants21@gmail.com>
2025-12-30 16:00:37 -0800

    Updated YouTubeLite.tsx

--- src/components/YouTubeLite.tsx
+++ src/components/YouTubeLite.tsx
@@ -76,9 +76,9 @@ const YouTubeLite: React.FC<YouTubeLiteProps> = ({
           />
 
           <div
-            className={`absolute inset-0  from-soot-950/90 via-soot-950/40 to-transparent transition-opacity duration-500 ${
+            className={`absolute inset-0 bg-gradient-to-t from-soot-950/90 via-soot-950/40 to-transparent transition-opacity duration-500 ${
               isHovered ? 'opacity-70' : 'opacity-50'
-            }`}
+            }`} 
           />
 
           <button
